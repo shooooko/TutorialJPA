b3c4dcb0e20fbac7f269ad7d4a7a9bf40fdbf7a0
shooooko <prp.ypp-n@rakumail.jp>
2025-01-21 16:53:51 +0900

    Lesson16課題提出

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -27,7 +27,7 @@ public class CountryController {
     }
 
     //------詳細画面------------
-    @GetMapping(value = {"/detail", "/detail/{code}/"})
+    @GetMapping(value = {"/detail", "/detail/{code}"})
     public String getCountry(@PathVariable(name = "code", required = false)String code, Model model) {
         //codeが指定されていたら検索結果、無ければ空のクラスを設定
         Country country = code !=null ? service.getCountry(code) : new Country();
@@ -47,14 +47,14 @@ public class CountryController {
     }
 
     //---------削除画面---------
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    @GetMapping(value = {"/delete", "/delete/{code}"})
+    public String deleteCountryForm(@PathVariable(name = "code", required = false)String code, Model model) {
         //country/delete.htmlに画面遷移
         return "country/delete";
     }
 
     //---------削除--------------
-    @PostMapping("/delete")
+    @PostMapping(value = {"/delete", "/delete/{code}"})
     public String deleteCountry(@RequestParam("code") String code, Model model) {
         //削除
         service.deleteCountry(code);
